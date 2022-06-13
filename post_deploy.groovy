c36fbc726f4a1f461061cf14e27b688a447bd14a
3axap <twofish@mail.ru>
2022-06-13 18:34:43 +0300

    add to vault2

--- post_deploy.groovy
+++ post_deploy.groovy
@@ -1,5 +1,5 @@
 def param_1 = "textext"
-def karta = ['vault1':[['ip':'10.20.30.40', 'port':'443'],['ip':'60.70.80.90', 'port':'8080']], 'vault2':['znachenie2','znachenie3']]
+def karta = ['vault1':[['ip':'10.20.30.40', 'port':'443'],['ip':'60.70.80.90', 'port':'8080']], 'vault2':[['ip':'xx.xx.xx.xx', 'port':'443'],['ip':'zz.zz.zz.zz', 'port':'8080']]]
 def command_a = "curl -vk https://www.google.com/"
 def command_b = "ls -la"
 def test_message_1 = 'ololo_${karta}'
